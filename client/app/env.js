681d0a46d5dd6ca07882a460969e866ddefae1e4
john87barker <john87barker@gmail.com>
2021-07-29 19:55:12 -0600

    Getting started. ENV and color scheme made

--- client/app/env.js
+++ client/app/env.js
@@ -1,5 +1,5 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
-export const domain = ''
-export const audience = ''
-export const clientId = ''
+export const domain = 'john87barker.us.auth0.com'
+export const audience = 'https://ReadIt.com'
+export const clientId = 'zuafSdmgkTEwtgJS64ZA8fFwXGpfSom5'
